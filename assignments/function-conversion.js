2ed117ba452daa10130f94474582aea947c06933
Rory Flint <rory@roryflint.co.uk>
2019-05-05 23:54:28 +0100

    MVP Complete

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,26 +5,48 @@
 // };
 // myFunction();
 
+let myFunction = () => console.log("Function was invoked!");
+
+console.log(myFunction());
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+
+let anotherFunction = (param) => {return param};
+
+console.log(anotherFunction(1234));
+
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+let add = (param1, param2) => param1+param2;
+
+console.log(add(12, 3));
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
 
+let subtract = (param1, param2) => param1-param2;
+
+console.log(subtract(45, 3));
+
 
 // Stretch
 
-// exampleArray = [1,2,3,4];
+exampleArray = [1,2,3,4];
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
 // console.log(triple);
+
+const triple = exampleArray.map((num) => num*3);
+
+console.log(triple);
+;
